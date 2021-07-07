db1ea9beb0239f188353de8457219e1ab2264c91
Roquigny Matthieu <matthieu.roquigny@gmail.com>
2021-07-07 23:54:36 +0200

    fix de la gestion de l'affichage easy admin

--- src/Controller/Admin/BienCrudController.php
+++ src/Controller/Admin/BienCrudController.php
@@ -3,61 +3,50 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Bien;
-use App\Entity\TypeBien;
 use App\Repository\BienRepository;
-use App\Repository\TypeBienRepository;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
-use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
 use Vich\UploaderBundle\Form\Type\VichImageType;
-use Symfony\Component\Security\Core\Security;
 
 class BienCrudController extends AbstractCrudController
 {
-
-    private $repoBien;
-
-    public function __construct(BienRepository $BienRepository)
-    {
-        $this->repoBien = $BienRepository;
-    }
-
     public static function getEntityFqcn(): string
     {
         return Bien::class;
     }
 
-   
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+
+        if ($this->isGranted('ROLE_PROPRIO')) {
+            $query = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+            $query->andWhere("entity.Proprietaire = :id")
+                ->setParameter('id', $this->getUser());
+            return $query;
+
+        } else if ($this->isGranted('ROLE_ADMIN')) {
+            $query = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
+            $query->getQuery();
+            return $query;
+        }
+    }
+
 
     public function configureFields(string $pageName): iterable
     {
         yield IdField::new('id')->onlyOnIndex();
         yield AssociationField::new('Type', 'Type de bien');
-
-        if ($this->isGranted('ROLE_PROPRIO')) {
-            yield AssociationField::new('Proprietaire')->setQueryBuilder(function(BienRepository $bienRepository) {
-                 $bienRepository->findById($this->getUser());
-            })
-            ;
-            
-            
-        } else if($this->isGranted('ROLE_ADMIN')) {
-            yield AssociationField::new('Proprietaire');
-        }
+        yield AssociationField::new('Proprietaire');
         yield TextField::new('imageFile', 'Image')
             ->hideOnIndex()
             ->setFormType(VichImageType::class);
